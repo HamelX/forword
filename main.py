cbb9d65f152dcd4fa11412b80c74a84b0324e629
HamelX <161572407+HamelX@users.noreply.github.com>
2026-06-08 10:19:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = discord.Client(intents=intents)
 
 SCRIM_CHANNEL_ID = 1511222831332069386
 COMPETITION_CHANNEL_ID = 1511222974164893776
+PRACTICE_CHANNEL_ID = 1513351268461187223
 CHAT_KR_CHANNEL_ID = 1493161282524938372
 CHAT_JP_CHANNEL_ID = 1493395325753364641
 SESH_BOT_ID = 616754792965865495
@@ -42,6 +43,18 @@ async def on_message(message):
         )
         await chat_jp.send(
             f"🏆 **新しい大会日程が登録されました！**\n"
+            f"👉 {message.jump_url}"
+        )
+
+    elif message.channel.id == PRACTICE_CHANNEL_ID:
+        chat_kr = client.get_channel(CHAT_KR_CHANNEL_ID)
+        chat_jp = client.get_channel(CHAT_JP_CHANNEL_ID)
+        await chat_kr.send(
+            f"🏃 **새 훈련 일정이 등록됐어요!**\n"
+            f"👉 {message.jump_url}"
+        )
+        await chat_jp.send(
+            f"🏃 **新しい練習日程が登録されました！**\n"
             f"👉 {message.jump_url}"
         )
 
